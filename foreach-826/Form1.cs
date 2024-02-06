dc6b2cb3940d4faeaad7ee80cfa98e02f6683cfe
kubrasayitass <145451189+kubrasayitass@users.noreply.github.com>
2024-02-06 12:55:15 +0300

    Add files via upload

---
+++ foreach-826/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace foreach_826
+{
+    
+    public partial class Form1 : Form
+    {
+        List<string> isimler = new List<string>() { "kübra", "nisa", "eymen", "efe" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string isim in isimler)
+            {
+                MessageBox.Show(isim);
+            }
+
+            
+        }
+    }
+}
